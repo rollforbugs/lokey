ca5053e333dcedecc3eb8e63722cdb6bf88deda8
Jonathan Zentgraf <zx96dev@gmail.com>
2017-04-08 13:37:24 -0400

    Not sure if better or worse

--- app/src/main/java/com/rollforbugs/shittykeyboard/ShittyIME.java
+++ app/src/main/java/com/rollforbugs/shittykeyboard/ShittyIME.java
@@ -1,9 +1,11 @@
 package com.rollforbugs.shittykeyboard;
 
+import android.graphics.drawable.Drawable;
 import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.media.AudioManager;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
@@ -13,6 +15,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+import java.util.logging.Logger;
 
 /**
  * Created by jonathan on 4/8/17.
@@ -23,24 +27,44 @@ public class ShittyIME extends InputMethodService implements KeyboardView.OnKeyb
     private KeyboardView kv;
     private Keyboard keyboard;
 
+    private int shuffleAmount = 6;
+    private Random rand = new Random();
     private boolean caps = false;
     private String keys = "abcdefghijklmnopqrstuvwxyz1234567890-=#@ :.,/";
 
+    private void swapKeys(Keyboard.Key k1, Keyboard.Key k2) {
+        int tmpX = k1.x;
+        int tmpY = k1.y;
+        int tmpWidth = k1.width;
+        int tmpHeight = k1.height;
+        int tmpGap = k1.gap;
+
+        k1.x = k2.x;
+        k1.y = k2.y;
+        k1.width = k2.width;
+        k1.height = k2.height;
+        k1.gap = k2.gap;
+
+        k2.x = tmpX;
+        k2.y = tmpY;
+        k2.width = tmpWidth;
+        k2.height = tmpHeight;
+        k2.gap = tmpGap;
+    }
+
+    private void shuffleKeyboard(Keyboard kbd) {
+        List<Keyboard.Key> keys = kbd.getKeys();
+        int nKeys = keys.size();
+        for (int i = 0; i < shuffleAmount; i++) {
+            swapKeys(keys.get(rand.nextInt(nKeys)), keys.get(rand.nextInt(nKeys)));
+        }
+    }
+
     @Override
     public View onCreateInputView() {
-        // Create a list of the keys and randomize order
-        ArrayList<Character> keyList = new ArrayList<>();
-        for (Character key : keys.toCharArray()) {
-            keyList.add(key);
-        }
-        Collections.shuffle(keyList);
-        StringBuffer sb = new StringBuffer();
-        for (Character key : keyList) {
-            sb.append(key);
-        }
-
         kv = (KeyboardView)getLayoutInflater().inflate(R.layout.keyboard, null);
-        keyboard = new Keyboard(this, R.xml.template, sb, 10, 2);
+        keyboard = new Keyboard(this, R.xml.qwerty);
+        shuffleKeyboard(keyboard);
         kv.setKeyboard(keyboard);
         kv.setOnKeyboardActionListener(this);
         return kv;
@@ -95,6 +119,8 @@ public class ShittyIME extends InputMethodService implements KeyboardView.OnKeyb
                         code = Character.toUpperCase(code);
                 ic.commitText(String.valueOf(code), 1);
         }
+        shuffleKeyboard(keyboard);
+        kv.invalidateAllKeys();
     }
 
     @Override
