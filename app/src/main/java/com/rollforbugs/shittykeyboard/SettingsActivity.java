ec7c3e65a50fb204048667aeae8506db7cc1c3c6
linkviii <linkeighth@gmail.com>
2017-04-08 20:54:04 -0400

    settings

---
+++ app/src/main/java/com/rollforbugs/shittykeyboard/SettingsActivity.java
@@ -0,0 +1,64 @@
+package com.rollforbugs.shittykeyboard;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+
+/**
+ * A {@link PreferenceActivity} that presents a set of application settings. On
+ * handset devices, settings are presented as a single list. On tablets,
+ * settings are split by category, with category headers shown to the left of
+ * the list of settings.
+ * <p>
+ * See <a href="http://developer.android.com/design/patterns/settings.html">
+ * Android Design: Settings</a> for design guidelines and the <a
+ * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
+ * API Guide</a> for more information on developing a Settings UI.
+ */
+public class SettingsActivity extends Activity {
+
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
+        }
+    }
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends PreferenceFragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.pref);
+        }
+
+        // @Override
+        // public View onCreateView(LayoutInflater inflater, ViewGroup
+        // container, Bundle savedInstanceState) {
+        // View rootView = inflater.inflate(R.layout.fragment_app_settings,
+        // container, false);
+        // return rootView;
+        // }
+    }
+
+
+}
