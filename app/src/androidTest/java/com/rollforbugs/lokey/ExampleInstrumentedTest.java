a8f231791147393734c0c00f8ee9617ac7848897
Jonathan Zentgraf <zx96dev@gmail.com>
2017-04-09 22:25:02 -0400

    Fix missed items for rebranding

--- app/src/androidTest/java/com/rollforbugs/lokey/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/rollforbugs/lokey/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.rollforbugs.shittykeyboard", appContext.getPackageName());
+        assertEquals("com.rollforbugs.lokey", appContext.getPackageName());
     }
 }
